bef94f7e7f3810059d7795b48dbfc4de5e08a051
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-03-23 21:05:51 -0700

    Adding ticket fields
    Adding ticket fields

--- booth-ui/src/components/Ticket.vue
+++ booth-ui/src/components/Ticket.vue
@@ -35,6 +35,26 @@
         />
       </div>
     </form>
+    
+     <button class="badge badge-primary mr-2"
+      v-if="currentTicket.status"
+      @click="updateStatus(false)"
+    >
+      Close Ticket
+    </button>
+    
+    <button v-else class="badge badge-primary mr-2"
+      @click="updateStatus(true)"
+    >
+      Open Ticket
+    </button>
+    
+     <button type="submit" class="badge badge-success"
+      @click="updateTicket"
+    >
+      Update
+    </button>
+    
     <p>{{ message }}</p>
   </div>
 
@@ -61,6 +81,37 @@ export default {
         .then(response => {
           this.currentTicket = response.data;
           console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    
+    updateStatus(status) {
+      var data = {
+        id: this.currentTicket.id,
+        subject: this.currentTicket.subject,
+        details: this.currentTicket.details,
+        user_id: this.currentTicket.user_id,
+        status: status
+      };
+
+      TicketDataService.update(this.currentTicket.id, data)
+        .then(response => {
+          console.log(response.data);
+          this.currentTicket.status = status;
+          this.message = 'The status was updated successfully!';
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+
+    updateTicket() {
+      TicketDataService.update(this.currentTicket.id, this.currentTicket)
+        .then(response => {
+          console.log(response.data);
+          this.message = 'The ticket was updated successfully!';
         })
         .catch(e => {
           console.log(e);
