6eba85eb907b9435f364e1cbfe90fd4b7fc8becc
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-03-24 19:28:07 -0700

    Added single role support

--- booth-server/app/middleware/verifySignUp.js
+++ booth-server/app/middleware/verifySignUp.js
@@ -36,13 +36,22 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
 
 checkRolesExisted = (req, res, next) => {
   if (req.body.roles) {
-    for (let i = 0; i < req.body.roles.length; i++) {
-      if (!ROLES.includes(req.body.roles[i])) {
-        res.status(400).send({
-          message: "Failed! Role does not exist = " + req.body.roles[i]
-        });
-        return;
+    if (typeof req.body.roles != "string") {
+      for (let i = 0; i < req.body.roles.length; i++) {
+        if (!ROLES.includes(req.body.roles[i])) {
+          res.status(400).send({
+            message: "Failed! Role does not exist = " + req.body.roles[i]
+          });
+          return;
+        }
       }
+    } else {
+      if (!ROLES.includes(req.body.roles)) {
+          res.status(400).send({
+            message: "Failed! Role does not exist = " + req.body.roles
+          });
+          return;
+        }
     }
   }
   
