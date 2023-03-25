0ed541217a0ee3a8165d2e42508bcf3e6ae2e809
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-03-25 04:36:09 -0700

    back

--- booth-server/server.js
+++ booth-server/server.js
@@ -5,8 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-	  origin: "http://localhost:8081",
-	  Access-Control-Allow-Private-Network: true
+	  origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
