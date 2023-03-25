f0589fe6be084fd04f4c4409feac19e45dfddb40
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-03-25 11:22:55 -0700

    g

--- booth-server/app/routes/user.routes.js
+++ booth-server/app/routes/user.routes.js
@@ -3,7 +3,7 @@ const controller = require("../controllers/user.controller");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://boothtickets.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
