07f606875494d80500dd4c18c88fe663ff032b95
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-03-25 08:08:15 -0700

    test

--- booth-server/server.js
+++ booth-server/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-	  origin: "https://boothtickets.com"
+	  origin: "*" //"https://boothtickets.com"
 };
 
 app.use(cors(corsOptions));
