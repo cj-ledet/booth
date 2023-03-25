6bb1e36b2dd473404a0e3dd01379fcc50c9c1897
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-03-25 02:44:03 -0700

    testing

--- booth-ui/src/services/user.service.js
+++ booth-ui/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/api/test/';
+const API_URL = 'http://http://ec2-54-241-133-168.us-west-1.compute.amazonaws.com:8080/api/test/';
 
 class UserService {
   getPublicContent() {
