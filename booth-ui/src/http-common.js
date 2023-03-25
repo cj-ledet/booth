21ac334b1f7d84909f16235cb8642b2592902144
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-03-25 04:15:00 -0700

    t

--- booth-ui/src/http-common.js
+++ booth-ui/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://ec2-54-241-133-168.us-west-1.compute.amazonaws.com:8080/api",
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
   }
