4256b2a35b5af094abba2b30e386574d0ec09cf3
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-03-24 21:00:46 -0700

    Adding new pages for routing

--- booth-ui/src/router.js
+++ booth-ui/src/router.js
@@ -1,8 +1,15 @@
 import { createWebHistory, createRouter } from "vue-router";
+import Home from "./components/Home.vue";
+import Login from "./components/Login.vue";
+import Register from "./components/Register.vue";
+// lazy-loaded
+const Profile = () => import("./components/Profile.vue")
+const BoardAdmin = () => import("./components/BoardAdmin.vue")
+const BoardModerator = () => import("./components/BoardModerator.vue")
+const BoardUser = () => import("./components/BoardUser.vue")
 
 const routes = [
     {
-      path: "/",
       alias: "/tickets",
       name: "tickets",
       component: () => import("./components/TicketsList")
@@ -16,7 +23,48 @@ const routes = [
       path: "/add",
       name: "add",
       component: () => import("./components/AddTicket")
-    }
+    },
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/home",
+      component: Home,
+    },
+    {
+      path: "/login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      component: Register,
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      // lazy-loaded
+      component: Profile,
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      // lazy-loaded
+      component: BoardAdmin,
+    },
+    {
+      path: "/mod",
+      name: "moderator",
+      // lazy-loaded
+      component: BoardModerator,
+    },
+    {
+      path: "/user",
+      name: "user",
+      // lazy-loaded
+      component: BoardUser,
+    },    
 ];
 
 const router = createRouter({
