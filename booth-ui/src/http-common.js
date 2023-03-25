b20b9cb940ca7d538f8987e7258c199de627367e
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-03-25 11:26:21 -0700

    t

--- booth-ui/src/http-common.js
+++ booth-ui/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://www.boothtickets.com/:8080/api",
+  baseURL: "https://www.boothtickets.com:8080/api",
   headers: {
     "Content-type": "application/json"
   }
