9e0439e41da35a88c76c8af0f50352471391dc8e
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-03-25 10:27:49 -0700

    t

--- booth-server/app/routes/user.routes.js
+++ booth-server/app/routes/user.routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     next();
   });
 
-  app.get("/api/test/all", controller.allAccess);
+  app.get("https://boothtickets.com/api/test/all", controller.allAccess);
 
   app.get(
     "/api/test/user",
