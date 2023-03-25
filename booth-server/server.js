6d767e7621703a1441ddfb6700b9a6bd644dfa68
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-03-25 11:36:08 -0700

    t

--- booth-server/server.js
+++ booth-server/server.js
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //simple route
 app.get("/", (req, res) => {
+	  res.setHeader('Access-Control-Allow-Origin', '*');
 	  res.json({message: "testing"});
 });
 
