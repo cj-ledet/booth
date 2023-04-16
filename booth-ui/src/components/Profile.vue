db5085538016308dfb0ee9d7a1e527e12984d6d8
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-04-16 16:48:21 -0700

    test
    test

--- booth-ui/src/components/Profile.vue
+++ booth-ui/src/components/Profile.vue
@@ -4,7 +4,6 @@
       <h3>
         <strong>{{currentUser.username}}</strong> Profile
       </h3>
-    </header>
     <p>
       <strong>Token:</strong>
       {{currentUser.accessToken.substring(0, 20)}} ... {{currentUser.accessToken.substr(currentUser.accessToken.length - 20)}}
@@ -21,6 +20,7 @@
     <ul>
       <li v-for="role in currentUser.roles" :key="role">{{role}}</li>
     </ul>
+    </header>
   </div>
 </template>
 
