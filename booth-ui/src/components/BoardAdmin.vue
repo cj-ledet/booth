e1011e08e7bde5ad624c1e4233ff7194a28cd99c
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-05-05 17:56:11 -0700

    fix

--- booth-ui/src/components/BoardAdmin.vue
+++ booth-ui/src/components/BoardAdmin.vue
@@ -25,7 +25,7 @@
           <label><strong>Email:</strong></label> {{ currentUser.email }}
         </div>
         
-        /*TODO Need to restructure backend admin API to accept user ID they would like to edit or view more in depth*/
+        <!--TODO Need to restructure backend admin API to accept user ID they would like to edit or view more in depth*/-->
         <a class="badge badge-warning"
           :href="'/admin/' + currentUser.id" 
         >
