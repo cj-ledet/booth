8af0a66583a3e4f7a7bf10b0491656be2447c6a5
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-05-05 17:49:41 -0700

    Testing admin board

--- booth-ui/src/components/BoardAdmin.vue
+++ booth-ui/src/components/BoardAdmin.vue
@@ -1,35 +1,91 @@
 <template>
-  <div class="container">
-    <header class="jumbotron">
-      <h3>{{ content }}</h3>
-    </header>
+  <div class="list row">
+    <div class="col-md-6">
+      <h4 style="color: white;">Users List</h4>
+      <ul class="list-group">
+        <li class="list-group-item"
+          :class="{ active: index == currentIndex }"
+          v-for="(user, index) in users"
+          :key="index"
+          @click="setActiveUser(user, index)"
+        >{{ user.id }}🎟 {{ user.email }}</li>
+      </ul>
+
+    </div>
+    <div class="col-md-6" style="color: white;">
+      <div v-if="currentUser">
+        <h4>User</h4>
+        <div>
+          <label><strong>User ID:</strong></label> {{ currentUser.id }}
+        </div>
+        <div>
+          <label><strong>Username:</strong></label> {{ currentUser.username }}
+        </div>
+        <div>
+          <label><strong>Email:</strong></label> {{ currentUser.email }}
+        </div>
+        
+        //TODO Need to restructure backend admin API to accept user ID they would like to edit or view more in depth
+        <a class="badge badge-warning"
+          :href="'/admin/' + currentUser.id" 
+        >
+          Edit
+        </a>
+      </div>
+      <div v-else>
+        <br />
+        <p style="color: white; font-size: 1.5em;">Select a Ticket...</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import UserService from "../services/user.service";
+import UserServive from "../services/user.service";
 
 export default {
-  name: "Admin",
+  name: "users-list",
   data() {
     return {
-      content: "",
+      users: [],
+      currentUser: null,
+      currentIndex: -1,
     };
   },
-  mounted() {
-    UserService.getAdminBoard().then(
-      (response) => {
-        this.content = response.data;
-      },
-      (error) => {
-        this.content =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
-      }
-    );
+  methods: {
+    retrieveUsers() {
+      UserServive.getAdminBoard()
+        .then(response => {
+          this.users = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+
+    //Not currently called but built for possible future need to refresh the list view (ex. deleting items)
+    refreshList() {
+      this.retrieveUsers();
+      this.currentUser = null;
+      this.currentIndex = -1;
+    },
+
+    setActiveUser(user, index) {
+      this.currentUser = user;
+      this.currentIndex = index;
+    }
   },
+  mounted() {
+    this.retrieveUsers();
+  }
 };
 </script>
+
+<style>
+.list {
+  text-align: left;
+  max-width: 750px;
+  margin: auto;
+}
+</style>
