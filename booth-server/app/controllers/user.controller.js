b784572b1404219ea8bb27ac15c46583d6583d7e
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-05-05 17:34:50 -0700

    Creating admin board results

--- booth-server/app/controllers/user.controller.js
+++ booth-server/app/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 const Ticket = db.tickets;
+const User = db.user;
 const Op = db.Sequelize.Op;
 
 exports.allAccess = (req, res) => {
@@ -11,22 +12,31 @@ exports.userBoard = (req, res) => {
 };
 
 exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
+  User.findAll()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+	message:
+	  err.message || "Some error occurred while retrieving tickets."
+      });
+    });
 };
 
 exports.moderatorBoard = (req, res) => {
   const user_id = req.query.user_id;
 
-	var condition = user_id ? { user_id: { [Op.iLike]: `%${user_id}%` } } : null;
+  var condition = user_id ? { user_id: { [Op.iLike]: `%${user_id}%` } } : null;
 
-	  Ticket.findAll({ where: condition })
-	    .then(data => {
-	      res.send(data);
-	    })
-	    .catch(err => {
-	      res.status(500).send({
-	        message:
-	          err.message || "Some error occurred while retrieving tickets."
-	      });
-	    });
+  Ticket.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+	message:
+	  err.message || "Some error occurred while retrieving tickets."
+      });
+    });
 };
