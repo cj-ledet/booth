6a0e055587bc9da0b72748dc280bc0a3b795d1c6
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-05-05 22:43:08 -0700

    Testing user role updates for admin

--- booth-server/app/controllers/user.controller.js
+++ booth-server/app/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 const Ticket = db.tickets;
 const User = db.user;
+const Role = db.role;
 const Op = db.Sequelize.Op;
 
 exports.allAccess = (req, res) => {
@@ -37,7 +38,7 @@ exports.adminBoardGetUser = (req, res) => {
       var authorities = [];
       user.getRoles().then(roles => {
         for (let i = 0; i < roles.length; i++) {
-          authorities.push("ROLE_" + roles[i].name.toUpperCase());
+          authorities.push(roles[i].name.toUpperCase());
         }
         res.status(200).send({
           id: user.id,
@@ -57,26 +58,112 @@ exports.adminBoardGetUser = (req, res) => {
 //Update a User by the id in the request
 exports.adminBoardUpdateUser = (req, res) => {
   const id = req.params.id;
-
-  User.update(req.body, {
-    where: { id: id }
-  })
+  
+  if (req.body == "addMod") {
+    Role.findByPk(2)
+      .then(role => {
+        User.addRole(role)
+	  .then(num => {
+            if (num == 1) {
+	      res.send({
+	        message: "Moderator permissions successfully added to this user!"
+	      });
+            } else {
+	      res.send({
+	        message: `Cannot update permissions for User with id=${id}. Maybe User was not found!`
+	      });
+            }
+          })
+          .catch(err => {
+            res.status(500).send({
+	      message: "Error updating permissions for User with id=" + id
+            });
+          });
+      })    
+  } else if (req.body == "removeMod") {
+    Role.findByPk(2)
+      .then(role => {
+        User.removeRole(role)
+	  .then(num => {
+            if (num == 1) {
+	      res.send({
+	        message: "Moderator permissions successfully removed from this user!"
+	      });
+            } else {
+	      res.send({
+	        message: `Cannot update permissions for User with id=${id}. Maybe User was not found!`
+	      });
+            }
+          })
+          .catch(err => {
+            res.status(500).send({
+	      message: "Error updating permissions for User with id=" + id
+            });
+          });
+      }) 
+  } else if (req.body == "addAdmin") {
+    Role.findByPk(3)
+      .then(role => {
+        User.addRole(role)
+	  .then(num => {
+            if (num == 1) {
+	      res.send({
+	        message: "Admin permissions successfully added to this user!"
+	      });
+            } else {
+	      res.send({
+	        message: `Cannot update permissions for User with id=${id}. Maybe User was not found!`
+	      });
+            }
+          })
+          .catch(err => {
+            res.status(500).send({
+	      message: "Error updating permissions for User with id=" + id
+            });
+          });
+      }) 
+  } else if (req.body == "removeAdmin") {
+    Role.findByPk(3)
+      .then(role => {
+        User.removeRole(role)
+	  .then(num => {
+            if (num == 1) {
+	      res.send({
+	        message: "Admin permissions successfully removed from this user!"
+	      });
+            } else {
+	      res.send({
+	        message: `Cannot update permissions for User with id=${id}. Maybe User was not found!`
+	      });
+            }
+          })
+          .catch(err => {
+            res.status(500).send({
+	      message: "Error updating permissions for User with id=" + id
+            });
+          });
+      }) 
+  } else {
+    User.update(req.body, {
+      where: { id: id }
+    })
     .then(num => {
       if (num == 1) {
-        res.send({
-          message: "User was updated successfully."
-        });
+	res.send({
+	  message: "User was updated successfully."
+	});
       } else {
-        res.send({
-          message: `Cannot update User with id=${id}. Maybe User was not found or req.body is empty!`
-        });
+	res.send({
+	  message: `Cannot update User with id=${id}. Maybe User was not found or req.body is empty!`
+	});
       }
     })
     .catch(err => {
       res.status(500).send({
-        message: "Error updating User with id=" + id
+	message: "Error updating User with id=" + id
       });
     });
+  }
 };
 
 exports.moderatorBoard = (req, res) => {
