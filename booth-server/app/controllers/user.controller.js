a7d7ebe8b925353516240e94b1d84891356fded5
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-05-05 18:16:52 -0700

    Built out ability for admin to update users

--- booth-server/app/controllers/user.controller.js
+++ booth-server/app/controllers/user.controller.js
@@ -24,6 +24,52 @@ exports.adminBoard = (req, res) => {
     });
 };
 
+//Find a single User with an id
+exports.adminBoardGetUser = (req, res) => {
+	  const id = req.params.id;
+
+	  User.findByPk(id)
+	   .then(data => {
+	      if (data) {
+	        res.send(data);
+	      } else {
+	        res.status(404).send({
+	          message: `Cannot find User with id=${id}.`
+	        });
+	      }
+	    })
+	    .catch(err => {
+	      res.status(500).send({
+	        message: "Error retrieving User with id=" + id
+	      });
+	    });
+};
+
+//Update a User by the id in the request
+exports.adminBoardUpdateUser = (req, res) => {
+  const id = req.params.id;
+
+  User.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "User was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update User with id=${id}. Maybe User was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating User with id=" + id
+      });
+    });
+};
+
 exports.moderatorBoard = (req, res) => {
   const user_id = req.query.user_id;
 
